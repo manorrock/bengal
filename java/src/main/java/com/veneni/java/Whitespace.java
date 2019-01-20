557288e292a1883e33c76a1d95363df6490897f1
Manfred Riem <mriem@veneni.com>
2019-01-20 09:21:40 -0700

    Fixes issue #3 - Add WhitespaceTransformer

---
+++ java/src/main/java/com/veneni/java/Whitespace.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2002-2019 Veneni.com. All Rights Reserved.
+ */
+package com.veneni.java;
+
+/**
+ * A (string of) whitespace.
+ * 
+ * @author Manfred Riem (mriem@veneni.com)
+ */
+public class Whitespace {
+    
+    /**
+     * Stores the whitespace.
+     */
+    private final String whitespace;
+
+    /**
+     * Constructor.
+     * 
+     * @param whitespace the whitespace. 
+     */
+    public Whitespace(String whitespace) {
+        this.whitespace = whitespace;
+    }
+}
