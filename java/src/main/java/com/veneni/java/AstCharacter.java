a9d2f573990d7751ac5b71f5f40f0b80df1955c1
Manfred Riem <mriem@veneni.com>
2019-01-25 16:08:11 -0700

    Fixes issue #8 - Add CharacterTransformer

---
+++ java/src/main/java/com/veneni/java/AstCharacter.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2002-2019 Veneni.com. All Rights Reserved.
+ */
+package com.veneni.java;
+
+/**
+ * An AST character.
+ * 
+ * @author Manfred Riem (mriem@veneni.com)
+ */
+public class AstCharacter {
+
+    /**
+     * Stores the character.
+     */
+    private final String character;
+    
+    /**
+     * Constructor.
+     * 
+     * @param character the character (in string format). 
+     */
+    public AstCharacter(String character) {
+        this.character = character;
+    }
+}
