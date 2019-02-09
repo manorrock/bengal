98bf033f5c5f87dfe4b576734a845758c9e0ea8a
Manfred Riem <mriem@veneni.com>
2019-02-09 08:56:28 -0700

    Fixes issue #7 - Add StringTransformer

---
+++ java/src/main/java/com/veneni/java/transformer/StringTransformer.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2002-2019 Veneni.com. All Rights Reserved.
+ */
+package com.veneni.java.transformer;
+
+import com.veneni.java.ast.AstString;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The string transformer.
+ *
+ * <p>
+ * This transformer will transform strings into AstStrings.
+ * </p>
+ *
+ * @author Manfred Riem (mriem@veneni.com)
+ */
+public class StringTransformer {
+
+    /**
+     * Stores the string builder.
+     */
+    private final StringBuilder stringBuilder;
+
+    /**
+     * Constructor.
+     */
+    public StringTransformer() {
+        stringBuilder = new StringBuilder();
+    }
+
+    /**
+     * Utility method to drain the StringBuilder back into a list.
+     *
+     * @param stringBuilder the string builder to drain.
+     * @return the list.
+     */
+    private List drainStringBuilderToList(StringBuilder stringBuilder) {
+        ArrayList result = new ArrayList();
+        if (stringBuilder.length() > 0) {
+            for (int i = 0; i < stringBuilder.length(); i++) {
+                result.add(stringBuilder.charAt(i));
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Transform.
+     *
+     * @param input the input.
+     * @return the output.
+     */
+    public List transform(List input) {
+        ArrayList result = new ArrayList();
+        for (Object object : input) {
+            if (object instanceof Character) {
+                Character character = (Character) object;
+                if (stringBuilder.length() == 0 && character == '"') {
+                    stringBuilder.append(character);
+                } else if (stringBuilder.length() > 0) {
+                    stringBuilder.append(character);
+                } else {
+                    result.addAll(drainStringBuilderToList(stringBuilder));
+                    result.add(object);
+                }
+                if (stringBuilder.length() > 1 && stringBuilder.toString().endsWith("\"")) {
+                    result.add(new AstString(stringBuilder.toString()));
+                }
+            } else {
+                result.addAll(drainStringBuilderToList(stringBuilder));
+                result.add(object);
+            }
+        }
+        result.addAll(drainStringBuilderToList(stringBuilder));
+        return result;
+    }
+}
