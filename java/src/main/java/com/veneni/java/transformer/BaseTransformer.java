a41eb1e336fe8ad540cc75966c390c9ba8edb448
Manfred Riem <mriem@veneni.com>
2019-02-13 07:01:01 -0700

    Fixes issue #9 - Refactor WhitespaceTransformer

---
+++ java/src/main/java/com/veneni/java/transformer/BaseTransformer.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2002-2019 Veneni.com. All Rights Reserved.
+ */
+package com.veneni.java.transformer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The base transformer class.
+ *
+ * @author Manfred Riem (mriem@veneni.com)
+ */
+public class BaseTransformer {
+
+    /**
+     * Utility method to drain a StringBuilder back into a list.
+     *
+     * @param stringBuilder the string builder to drain.
+     * @return the list.
+     */
+    protected List drainStringBuilderToList(StringBuilder stringBuilder) {
+        ArrayList result = new ArrayList();
+        if (stringBuilder.length() > 0) {
+            for (int i = 0; i < stringBuilder.length(); i++) {
+                result.add(stringBuilder.charAt(i));
+            }
+        }
+        return result;
+    }
+}
