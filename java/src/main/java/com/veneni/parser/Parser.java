eeb3ef5408ba12b2e899028c6c54f99564b879e5
Manfred Riem <mriem@veneni.com>
2019-03-19 17:04:33 -0600

    Fixes issue #17 - Create Veneni Parser

---
+++ java/src/main/java/com/veneni/parser/Parser.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2002-2019 Veneni.com. All Rights Reserved.
+ */
+package com.veneni.parser;
+
+import java.util.List;
+
+/**
+ * The Parser.
+ * 
+ * @author Manfred Riem (mriem@veneni.com)
+ */
+public class Parser {
+
+    /**
+     * Parse.
+     * 
+     * @param input the input list.
+     * @return the output.
+     */
+    public Object parse(List<Object> input) {
+        return null;
+    }
+}
