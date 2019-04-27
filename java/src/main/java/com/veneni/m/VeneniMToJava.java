c5d940be494481f02334ba1e53f41e4a8a1b26c7
Manfred Riem <mriem@veneni.com>
2019-04-27 09:45:35 -0600

    Fixes issue #32 - Add Java generator for Veneni M files

---
+++ java/src/main/java/com/veneni/m/VeneniMToJava.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2002-2019 Veneni.com. All Rights Reserved.
+ */
+package com.veneni.m;
+
+/**
+ * Translate Veneni M to Java.
+ * 
+ * @author Manfred Riem (mriem@veneni.com)
+ */
+public class VeneniMToJava {
+    
+    /**
+     * Main method.
+     * 
+     * @param arguments the arguments.
+     */
+    public static void main(String[] arguments) {
+        System.out.println("Veneni M to Java");
+    }
+}
