ab87b0e43d87e0b71342f13b5945129ca4ac9b2c
Manfred Riem <mriem@veneni.com>
2019-10-04 16:51:28 -0600

    Fixes issue #4 - Parse 'object' keyword

---
+++ java/src/main/java/com/veneni/m/Keyword.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2002-2019 Veneni.com. All Rights Reserved.
+ */
+package com.veneni.m;
+
+/**
+ * The enum containing all of the Veneni M keywords.
+ *
+ * @author Manfred Riem (mriem@veneni.com)
+ */
+public enum Keyword {
+    /**
+     * The "object" keyword.
+     */
+    OBJECT
+}
--- java/src/main/java/com/veneni/m/Parser.java
+++ java/src/main/java/com/veneni/m/Parser.java
@@ -5,18 +5,21 @@ package com.veneni.m;
 
 /**
  * The parser.
- * 
+ *
  * @author Manfred Riem (mriem@veneni.com)
  */
 public class Parser {
 
     /**
      * Parse the input.
-     * 
+     *
      * @param input the input.
      * @return the output.
      */
-	public Object parse(String input) {
-		return input;
-	}
+    public Object parse(String input) {
+        if (input.equals("object")) {
+            return Keyword.OBJECT;
+        }
+        return input;
+    }
 }
