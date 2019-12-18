dbc80cddee1a118859f23450088c617ae9b6f802
Manfred Riem <mriem@veneni.com>
2019-12-18 16:23:54 -0700

    Fixes issue #17 - Create JavaCompiler class

---
+++ java/src/main/java/com/veneni/m/JavaCompiler.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2002-2019 Veneni.com. All Rights Reserved.
+ */
+package com.veneni.m;
+
+/**
+ * The Veneni M Java compiler.
+ * 
+ * @author Manfred Riem (mriem@manorrock.com)
+ */
+public class JavaCompiler {
+    
+    /**
+     * Main method.
+     * 
+     * @param arguments the arguments.
+     */
+    public static void main(String[] arguments) {
+        System.out.println("JavaCompiler.main");
+    }
+}
