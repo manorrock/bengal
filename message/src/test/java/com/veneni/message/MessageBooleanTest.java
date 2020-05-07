2fed8c6d7c778e7d705bb01c3265ff0ca9ba444b
Manfred Riem <mriem@veneni.com>
2020-05-07 16:47:19 -0600

    Fixes issue #39 - Add false to Message compiler

--- message/src/test/java/com/veneni/message/MessageBooleanTest.java
+++ message/src/test/java/com/veneni/message/MessageBooleanTest.java
@@ -4,6 +4,7 @@
 package com.veneni.message;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
@@ -24,7 +25,21 @@ public class MessageBooleanTest {
         Object result = parser.parse(string);
         assertTrue(result instanceof MessageBoolean);
         MessageBoolean mb = (MessageBoolean) result;
-        assertEquals(true, mb.getBoolean());
+        assertTrue(mb.getBoolean());
         assertEquals("true", mb.toJava());
+    }
+    
+    /**
+     * Test translation "false" to "false".
+     */
+    @Test
+    public void testTranslatingFalse() {
+        String string = "false";
+        VeneniMessageParser parser = new VeneniMessageParser();
+        Object result = parser.parse(string);
+        assertTrue(result instanceof MessageBoolean);
+        MessageBoolean mb = (MessageBoolean) result;
+        assertFalse(mb.getBoolean());
+        assertEquals("false", mb.toJava());
     }
 }
