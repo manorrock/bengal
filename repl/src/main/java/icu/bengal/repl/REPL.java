c0e413b6da1bd9fd10294a782039a77e376a6b4d
Manfred Riem <mriem@manorrock.com>
2023-03-24 17:06:45 -0500

    Fixes issue #9 - Add GraalVM to REPL module (#11)

---
+++ repl/src/main/java/icu/bengal/repl/REPL.java
@@ -0,0 +1,18 @@
+package icu.bengal.repl;
+
+/**
+ * The main entry point.
+ * 
+ * @author Manfred Riem (manfred@bengal.icu)
+ */
+public class REPL {
+
+    /**
+     * The main method.
+     * 
+     * @param arguments the command line arguments
+     */
+    public static void main(String[] arguments) {
+        System.out.println("Welcome to Bengal REPL");
+    }
+}
