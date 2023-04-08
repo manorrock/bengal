a212f12d9856a953f7444162a1821bb392fd3f96
Manfred Riem <mriem@manorrock.com>
2023-04-08 07:02:37 -0500

    Fixes issue #14 - Add banner (#17)

--- repl/src/main/java/icu/bengal/repl/REPL.java
+++ repl/src/main/java/icu/bengal/repl/REPL.java
@@ -4,25 +4,36 @@ import java.util.Scanner;
 
 /**
  * The main entry point.
- * 
+ *
  * @author Manfred Riem (manfred@bengal.icu)
  */
 public class REPL {
-    
+
     /**
      * Stores the done flag.
      */
     private boolean done;
-    
+
     /**
      * Stores the line.
      */
     private String line;
-    
+
     /**
      * Stores the result.
      */
     private String result;
+
+    /**
+     * Show the banner.
+     */
+    private void banner() {
+        System.out.println(
+                """
+-------------------------------------------------------------------------------
+  Bengal REPL - Enjoy!
+-------------------------------------------------------------------------------""");
+    }
 
     /**
      * Evaluate.
@@ -30,21 +41,21 @@ public class REPL {
     private void eval() {
         result = line;
     }
-    
+
     /**
      * Print
      */
     private void print() {
         System.out.println(result);
     }
-    
+
     /**
      * Prompt
      */
     private void prompt() {
         System.out.print("$ ");
     }
-    
+
     /**
      * Read.
      */
@@ -52,12 +63,13 @@ public class REPL {
         Scanner scanner = new Scanner(System.in);
         line = scanner.nextLine();
     }
-    
+
     /**
      * REPL loop.
      */
     private void run() {
-        while(!done) {
+        banner();
+        while (!done) {
             prompt();
             read();
             eval();
@@ -67,7 +79,7 @@ public class REPL {
 
     /**
      * The main method.
-     * 
+     *
      * @param arguments the command line arguments
      */
     public static void main(String[] arguments) {
