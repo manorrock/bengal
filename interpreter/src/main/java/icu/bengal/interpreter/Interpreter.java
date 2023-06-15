a582cf44521d15194a6b1a3965c8049000ba0ff2
Manfred Riem <mriem@manorrock.com>
2023-06-15 07:31:47 -0500

    Fixes issue #37 - Create interpreter module (#38)

---
+++ interpreter/src/main/java/icu/bengal/interpreter/Interpreter.java
@@ -0,0 +1,19 @@
+package icu.bengal.interpreter;
+
+/**
+ * The Bengal Interpreter.
+ * 
+ * @author Manfred Riem (mriem@manorrock.com)
+ */
+public class Interpreter {
+    
+    /**
+     * Main method.
+     * 
+     * @param arguments the command-line arguments. 
+     */
+    public static void main(String[] arguments) {
+        System.out.println("Bengal Interpreter");
+        System.out.println();
+    }
+}
