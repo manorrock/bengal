4ed406ed4fb8bfe1c4b5dcbf30ab9da088fb5823
Manfred Riem <mriem@manorrock.com>
2023-08-10 16:06:26 -0500

    Fixes #68 - Add ability to read input from standard input (#71)

--- repl/src/main/java/icu/bengal/repl/REPL.java
+++ repl/src/main/java/icu/bengal/repl/REPL.java
@@ -97,6 +97,9 @@ public class REPL {
             read();
             eval();
             print();
+            if (lines.isEmpty()) {
+                done = true;
+            }
         }
     }
 
