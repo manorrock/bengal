8f4e4406a3ed0a50eff39e4be16bfd224eb79769
Manfred Riem <mriem@manorrock.com>
2023-09-19 16:07:05 -0500

    Fixes #88 - Add internal data structure for classes (#90)

---
+++ repl/src/main/java/icu/bengal/repl/BengalClass.java
@@ -0,0 +1,32 @@
+package icu.bengal.repl;
+
+/**
+ * A class in the Bengal ecosystem.
+ * 
+ * @author Manfred Riem (mriem@manorrock.com)
+ */
+public class BengalClass {
+    
+    /**
+     * Stores the name.
+     */
+    private String name;
+
+    /**
+     * Get the name.
+     * 
+     * @return the name.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set the name.
+     * 
+     * @param name the name.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
