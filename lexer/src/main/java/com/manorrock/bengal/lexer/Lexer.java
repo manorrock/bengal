ecfc353b4fc9c473ec220a2a5ce48d0f70237532
Manfred Riem <mriem@manorrock.com>
2024-03-24 10:01:36 -0500

    Fixes #5 - Add lexer (#6)

---
+++ lexer/src/main/java/com/manorrock/bengal/lexer/Lexer.java
@@ -0,0 +1,17 @@
+package com.manorrock.bengal.lexer;
+
+/**
+ * The Bengal Lexer.
+ * 
+ * @author Manfred Riem (mriem@manorrock.com)
+ */
+public class Lexer {
+
+    /**
+     * Main method.
+     * 
+     * @param arguments the command line arguments
+     */
+    public static void main(String[] arguments) {
+    }
+}
