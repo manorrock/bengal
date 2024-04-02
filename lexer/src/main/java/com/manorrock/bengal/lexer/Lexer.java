3e444cbe8a842e5abbaa08ec65b4b45d332bacd6
Manfred Riem <mriem@manorrock.com>
2024-04-02 16:11:42 -0500

    Fixes #9 - Tokenize semi colons

--- lexer/src/main/java/com/manorrock/bengal/lexer/Lexer.java
+++ lexer/src/main/java/com/manorrock/bengal/lexer/Lexer.java
@@ -1,11 +1,21 @@
 package com.manorrock.bengal.lexer;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+
 /**
  * The Bengal Lexer.
  * 
  * @author Manfred Riem (mriem@manorrock.com)
  */
 public class Lexer {
+    
+    /**
+     * Stores the semi colon constant.
+     */
+    private static String SEMI_COLON = ";";
 
     /**
      * Main method.
@@ -13,5 +23,19 @@ public class Lexer {
      * @param arguments the command line arguments
      */
     public static void main(String[] arguments) {
+        File inputFilename = new File(arguments[0]);
+        try (LineNumberReader reader = new LineNumberReader(new FileReader(inputFilename))) {
+            while(reader.ready()) {
+                int character = reader.read();
+                if (character == ';') {
+                    LexerToken token = new LexerToken(SEMI_COLON, reader.getLineNumber());
+                    System.out.println(token);
+                } else {
+                    System.out.println((char) character);
+                }
+            }
+        } catch(IOException ioe) {
+            
+        }
     }
 }
