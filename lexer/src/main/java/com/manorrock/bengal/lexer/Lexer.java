3f75793b316a66ccfb0fc116cc7c10d2e4bda9be
Manfred Riem <mriem@manorrock.com>
2024-04-07 10:18:58 -0500

    Fixes #15 - Tokenize class keyword

--- lexer/src/main/java/com/manorrock/bengal/lexer/Lexer.java
+++ lexer/src/main/java/com/manorrock/bengal/lexer/Lexer.java
@@ -7,21 +7,31 @@ import java.io.LineNumberReader;
 
 /**
  * The Bengal Lexer.
- * 
+ *
  * @author Manfred Riem (mriem@manorrock.com)
  */
 public class Lexer {
     
     /**
+     * Stores the buffer.
+     */
+    private static StringBuilder buffer = new StringBuilder();
+
+    /**
+     * Stores the 'class' keyword constant.
+     */
+    private static String CLASS_KEYWORD = "class";
+
+    /**
      * Stores the left parenthesis constant.
      */
     private static String LEFT_PARENTHESIS = "(";
-    
+
     /**
      * Stores the right parenthesis constant.
      */
     private static String RIGHT_PARENTHESIS = ")";
-    
+
     /**
      * Stores the semi colon constant.
      */
@@ -29,32 +39,65 @@ public class Lexer {
 
     /**
      * Main method.
-     * 
+     *
      * @param arguments the command line arguments
      */
     public static void main(String[] arguments) {
         File inputFilename = new File(arguments[0]);
         try (LineNumberReader reader = new LineNumberReader(new FileReader(inputFilename))) {
-            while(reader.ready()) {
+            while (reader.ready()) {
                 int character = reader.read();
                 switch (character) {
-                    case ';' ->                         {
-                            LexerToken token = new LexerToken(SEMI_COLON, reader.getLineNumber());
-                            System.out.println(token);
+                    case ';' -> {
+                        LexerToken token = new LexerToken(SEMI_COLON, reader.getLineNumber());
+                        System.out.println(token);
+                    }
+                    case '(' -> {
+                        LexerToken token = new LexerToken(LEFT_PARENTHESIS, reader.getLineNumber());
+                        System.out.println(token);
+                    }
+                    case ')' -> {
+                        LexerToken token = new LexerToken(RIGHT_PARENTHESIS, reader.getLineNumber());
+                        System.out.println(token);
+                    }
+                    case 'a' -> {
+                        if (buffer.toString().startsWith("cl")) {
+                            buffer.append("a");
+                        } else {
+                            System.out.println(buffer.toString());
+                            buffer = new StringBuilder();
                         }
-                    case '(' ->                         {
-                            LexerToken token = new LexerToken(LEFT_PARENTHESIS, reader.getLineNumber());
-                            System.out.println(token);
+                    }
+                    case 'c' -> {
+                        buffer.append("c");
+                    }
+                    case 'l' -> {
+                        if (buffer.toString().startsWith("c")) {
+                            buffer.append("l");
+                        } else {
+                            System.out.println(buffer.toString());
+                            buffer = new StringBuilder();
                         }
-                    case ')' ->                         {
-                            LexerToken token = new LexerToken(RIGHT_PARENTHESIS, reader.getLineNumber());
-                            System.out.println(token);
+                    }
+                    case 's' -> {
+                        if (buffer.toString().startsWith("cla")) {
+                            buffer.append("s");
+                            if (buffer.toString().equals("class")) {
+                                LexerToken token = new LexerToken(CLASS_KEYWORD, reader.getLineNumber());
+                                System.out.println(token);
+                                buffer = new StringBuilder();
+                            }
+                        } else {
+                            System.out.println(buffer.toString());
+                            buffer = new StringBuilder();
                         }
-                    default -> System.out.println((char) character);
+                    }
+                    default ->
+                        System.out.println((char) character);
                 }
             }
-        } catch(IOException ioe) {
-            
+        } catch (IOException ioe) {
+
         }
     }
 }
