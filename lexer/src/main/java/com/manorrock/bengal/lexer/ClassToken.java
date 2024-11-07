7215b25f276d2bade779aed2932ad0de2f070e7f
Manfred Riem <mriem@manorrock.com>
2024-11-07 17:35:21 -0600

    Fixes #62 - Implement support for 'class' keyword (#84)

---
+++ lexer/src/main/java/com/manorrock/bengal/lexer/ClassToken.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2002-2024 Manorrock.com. All Rights Reserved.
+ */
+package com.manorrock.bengal.lexer;
+
+/**
+ * The 'class' token.
+ * 
+ * @author Manfred Riem (mriem@manorrock.com)
+ */
+public class ClassToken extends Token {
+
+    /**
+     * Constructor.
+     */
+    public ClassToken() {
+    }
+}
